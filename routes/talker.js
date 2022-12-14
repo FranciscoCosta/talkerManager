59d9c2f2984666f67a2b6a9d25ae564c4a4d2286
Xico <francisco100eg@gmail.com>
2022-12-14 15:54:06 -0300

    Feat: Requisito 2

--- routes/talker.js
+++ routes/talker.js
@@ -10,4 +10,14 @@ talkerRouter.get('/talker', async (_req, res) => {
     return res.status(200).json(talkers);
 });
 
+talkerRouter.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talk = await readFile();
+    const talkid = talk.find((talker) => talker.id === Number(id));
+    console.log(talkid);
+    if (!talkid) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
+}
+    return res.status(200).json(talkid);
+});
 module.exports = talkerRouter;
