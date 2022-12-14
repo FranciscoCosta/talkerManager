3a298122018d77f0ea8cdc0d4ea31435dae238c2
Xico <francisco100eg@gmail.com>
2022-12-14 16:37:28 -0300

    Feat: Requisito 4

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { validateUser } = require('../middlewares/validateUser');
 const { geraToken } = require('../utils');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/login', (req, res) => {
+loginRouter.post('/login', validateUser, (req, res) => {
     const token = { token: geraToken() };
     res.status(200).json(token);
 });
